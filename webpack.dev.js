ed8dd7ca86e1dc2d26df732d35e46074e549af89
changminjie <changminjie@meituan.com>
2021-07-07 14:23:46 +0800

    init

---
+++ webpack.dev.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+  entry: './test/index.js',
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        includes: /test\//,
+        test: /\.js/,
+        use: {
+          presets: ['env'],
+          plugins: [['console-omit', {env: 'production'}], 'lodash-import']
+        }
+      }
+    ]
+  }
+}
